db7517e34e48ef042170b90e4d4fa6efdbfa1cd4
João Ventura <joaojonesventura@gmail.com>
2022-09-21 10:34:21 +0000

    Test describe-instance-information not yet implemented

--- tests/integration/test_ssm.py
+++ tests/integration/test_ssm.py
@@ -144,3 +144,9 @@ class TestSSM:
         assert found_param["ARN"]
         assert found_param["Type"] == "String"
         assert found_param["Value"] == "value"
+
+    @pytest.mark.aws_validated
+    def test_describe_instance_information(self, ssm_client):
+        with pytest.raises(ssm_client.exceptions.ClientError) as exc:
+            ssm_client.describe_instance_information()
+        assert "not yet implemented" in exc.value.response["Error"]["Message"]
